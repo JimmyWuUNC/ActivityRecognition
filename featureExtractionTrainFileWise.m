f779770a6fe43ac91e6369ecce1b699229f64129
James Stevens <a0135636@u.nus.edu>
2015-08-04 19:14:57 -0400

    Added in timeExcercisedInSeconds variable.
    Unclear what we want to do with this variable. Are just supposed to print it to standard output?

--- featureExtractionTrainFileWise.m
+++ featureExtractionTrainFileWise.m
@@ -11,6 +11,7 @@ for masterVariable=1:nargin;%Goes over all the files
     windowWidth=64;%vary this to vary the size of the window
     windowOverlap=0.5;
     windowStep=windowWidth*windowOverlap;
+    sampling_rate=25; %based on experimentation
     table_x1 = [];
     table_y1 = [];
     table_z1 = [];
@@ -26,6 +27,7 @@ for masterVariable=1:nargin;%Goes over all the files
 
     [rows,~] = size(table_x1);
     
+    timeSpentExcercisingInSeconds = rows / samplingRate;
     
     %Step 1a: Calculate acceleration for the data
     table_accel1=zeros(rows,1); %Pre-allocate an array of all zeroes to save compilation time.
